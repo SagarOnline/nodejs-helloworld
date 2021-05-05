2a53f9f49606f3099526ac3bb79f44120a3824ec
schopade <schopade@slb.com>
2021-05-06 03:55:24 +0530

    added docs api

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/docs', (req, res) => {
+  res.send('Swagger Docs!')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
